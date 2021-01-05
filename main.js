4f7f7b1316a3afc9137fddd810df220094003154
Raj Rajput <rajput0@sheridancollege.ca>
2021-01-05 00:34:02 -0500

    Language changed to sanskrit/Minor changes in style

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
 "use strict";
 
+function replaceEnglish(str){
+  let res = str
+  res = res.toString().replace(/1/g,"१");
+  res = res.toString().replace(/2/g,"२");
+  res = res.toString().replace(/3/g,"३");
+  res = res.toString().replace(/4/g,"४");
+  res = res.toString().replace(/5/g,"५");
+  res = res.toString().replace(/6/g,"६");
+  res = res.toString().replace(/7/g,"७");
+  res = res.toString().replace(/8/g,"८");
+  res = res.toString().replace(/9/g,"९");
+  console.log("res: " +  res)
+  return res;
+  
+}
+
+
 const input = document.querySelector(".input");
 const result = document.querySelector(".result");
 const deleteBtn = document.querySelector(".delete");
 const keys = document.querySelectorAll(".bottom span");
 
-let operation = "";
-let answer;
-let decimalAdded = false;
+var operation = "";
+var answer;
+var decimalAdded = false;
 
 const operators = ["+", "-", "x", "÷"];
 
 function handleKeyPress (e) {
   const key = e.target.dataset.key;
+  console.log(key)
   const lastChar = operation[operation.length - 1];
+  console.log(lastChar)
 
   if (key === "=") {
     return;
@@ -29,25 +48,25 @@ function handleKeyPress (e) {
 
   if (operation.length === 0 && key === "-") {
     operation += key;
-    input.innerHTML = operation;
+    input.innerHTML = replaceEnglish(operation);
     return;
   }
 
   if (operation.length === 0 && operators.indexOf(key) !== -1) {
-    input.innerHTML = operation;
+    input.innerHTML = replaceEnglish(operation);
     return;
   }
 
   if (operators.indexOf(lastChar) !== -1 && operators.indexOf(key) !== -1) {
     operation = operation.replace(/.$/, key);
-    input.innerHTML = operation;
+    input.innerHTML = replaceEnglish(operation);
     return;
   }
 
   if (key) {
     if (key === ".") decimalAdded = true;
     operation += key;
-    input.innerHTML = operation;
+    input.innerHTML = replaceEnglish(operation);
     return;
   }
 
@@ -63,7 +82,7 @@ function evaluate(e) {
 
   if (operation.length === 0) {
     answer = "";
-    result.innerHTML = answer;
+    result.innerHTML = replaceEnglish(answer);
     return;
   }
 
@@ -80,17 +99,17 @@ function evaluate(e) {
       decimalAdded = false;
       operation = `${answer}`;
       answer = "";
-      input.innerHTML = operation;
-      result.innerHTML = answer;
+      input.innerHTML = replaceEnglish(operation);
+      result.innerHTML = replaceEnglish(answer);
       return;
     }
 
-    result.innerHTML = answer;
+    result.innerHTML = replaceEnglish(answer);
 
   } catch (e) {
     if (key === "=") {
       decimalAdded = false;
-      input.innerHTML = `<span class="error">${operation}</span>`;
+      input.innerHTML = `<span class="error">${replaceEnglish(operation)}</span>`;
       result.innerHTML = `<span class="error">Bad Expression</span>`;
     }
     console.log(e);
@@ -103,17 +122,22 @@ function clearInput (e) {
   if (e.ctrlKey) {
     operation = "";
     answer = "";
-    input.innerHTML = operation;
-    result.innerHTML = answer;
+    input.innerHTML = replaceEnglish(operation);
+    result.innerHTML = replaceEnglish(answer);
     return;
   }
 
   operation = operation.slice(0, -1);
-  input.innerHTML = operation;
+  input.innerHTML = replaceEnglish(operation);
 
 }
 
+function clearEverything(){
+  location.reload()
+}
+
 deleteBtn.addEventListener("click", clearInput);
+deleteBtn.addEventListener("dblclick", clearEverything);
 keys.forEach(key => {
   key.addEventListener("click", handleKeyPress);
   key.addEventListener("click", evaluate);
